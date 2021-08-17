54cf2d097c0e49f2b68cf3e93f817849af4b7c14
Yavuz <yavuz.orbey@gmail.com>
2021-08-17 11:40:13 -0400

    initial commit

---
+++ js-object-examples.js
@@ -0,0 +1,16 @@
+const obj = {
+"name": "Yavuz",
+"birthday": 01010001,
+"cities": ["Philadelphia", "Truth and Consequences"],
+"isHuman": true,
+"planetsVisited": null,
+"preferences": {
+    "favoriteMovies": ["Big Trouble in Little China"]
+}
+};
+console.log(obj)
+
+console.log(JSON.stringify(obj));
+
+const JSONString = "{\"name\": \"Ben\"}";
+console.log(JSONString);
