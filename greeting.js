c33018813084673f5a0c91433bc222647b4c0a40
Yavuz <yavuz.orbey@gmail.com>
2021-08-17 14:46:11 -0400

    add greeting.js

---
+++ greeting.js
@@ -0,0 +1,56 @@
+/*
+Use the strings in the arrays to form a sentence like: "Hi, my name is <Name>
+Be sure to have beginning words of sentences with the proper case.
+
+Console log the result
+
+Use a different greeting word for each gender
+Use "his" for men, "her" for women, "their" for nonbinary.
+
+Use "my" if the isSelf argument is true
+
+
+*/
+
+/**
+ @param {string} name
+        name of person
+ @param  {string} gender
+        Possible values of "gender" argument are "male", "female", "nonbinary"
+ @param {boolean} isSelf
+ @returns {string}
+          greeting sentence
+*/
+
+const greetingWords = ["hello","hi","hey"];
+
+const prefixes = ["my", "his", "her", "their"];
+
+function capitalize(text){
+    return text[0].toUpperCase() + text.substr(1, greeting.length-1)
+}
+function greeting(name, gender, isSelf) {
+    // do stuff here ...
+    let greeting ="";
+    let prefix ="";
+    switch(gender) {
+        case "male":
+            greeting = greetingWords[0]
+            prefix =prefixes[1];
+            break;
+        case "female":
+            greeting = greetingWords[1]
+            prefix =prefixes[2];
+            break;
+        default:
+            greeting = greetingWords[2]
+            prefix =prefixes[3];
+    }
+    prefix = isSelf ? prefixes[0] : prefix;
+    return  `${capitalize(greeting)}, ${prefix} name is ${name}`;
+}
+console.log(greeting("Jim", "male", false))
+console.log(greeting("John", "male", true))
+console.log(greeting("Jill", "female", false))
+console.log(greeting("Jake", "sadasdasdfas", true))
+console.log(greeting("Judith", "fsdfsdfsdvc", false))
--- js-object-examples.js
+++ js-object-examples.js
@@ -1,4 +1,4 @@
-const obj = {
+/* const obj = {
 "name": "Yavuz",
 "birthday": 01010001,
 "cities": ["Philadelphia", "Truth and Consequences"],
@@ -14,3 +14,23 @@ console.log(JSON.stringify(obj));
 
 const JSONString = "{\"name\": \"Ben\"}";
 console.log(JSONString);
+
+
+function sayHi(name){
+    return `my name is ${name}`
+}
+
+console.log(sayHi("Yavuz"));
+
+
+const greetingWords = ["hello","hi","hey"];
+const possesives = ["my","mine","his","her"];
+//create a variable with your name here
+
+let myname = "Yavuz"
+function greeting() {
+  return  `${greetingWords[1]} ${possesives[0]} name is ${myname}`;
+}
+
+console.log("-----------")
+console.log(greeting()) */
